805aefe573fbb8d5ccdd5c6acf68b6c91790f74a
UnknownVisuals <reynaldig40@gmail.com>
2023-09-04 23:04:23 +0700

    Save 02 - GUI Intro

---
+++ myFirstProgram/src/Expression.java
@@ -0,0 +1,9 @@
+public class Expression {
+    public static void main(String[] args) {
+        double friends = 10;
+
+        friends /= (double) 3;
+
+        System.out.println(friends);
+    }
+}
